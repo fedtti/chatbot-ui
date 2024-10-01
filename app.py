b3c314cd4f5a89195b600045dc68ccc0906173b7
Federico Moretti <hello@federicomoretti.it>
2024-10-01 19:18:50 +0200

    feat: add OpenAI support, CSS stylings, and HTML markup

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify, render_template
+import os
+from dotenv import load_dotenv
 
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
+load_dotenv()
+GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+
+history = []
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+
+    return render_template('index.html', history=history)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
